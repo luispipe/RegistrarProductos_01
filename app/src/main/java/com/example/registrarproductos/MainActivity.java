af9229f3e84a50097b0f47e0986ec57759ce3fc7
luispipe <lflondonor@unal.edu.co>
2023-10-25 09:54:49 -0300

    Second commit

--- app/src/main/java/com/example/registrarproductos/MainActivity.java
+++ app/src/main/java/com/example/registrarproductos/MainActivity.java
@@ -3,12 +3,88 @@ package com.example.registrarproductos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText name,quantity,prize;
+    Button register, close;
+    TableLayout table;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name= findViewById(R.id.editTextProducto);
+        quantity=findViewById(R.id.editTextCantidad);
+        prize=findViewById(R.id.editTextPrecio);
+
+        register=findViewById(R.id.buttonRegistrar);
+        close= findViewById(R.id.buttonFinalizar);
+        table= findViewById(R.id.tableProducts);
+
+        ArrayList<Producto> canasta= new ArrayList<>();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombre= name.getText().toString();
+                String cantidad= quantity.getText().toString();
+                String precio= prize.getText().toString();
+                if(nombre.isEmpty() || cantidad.isEmpty() || precio.isEmpty()){
+                    Toast.makeText(getApplicationContext(),
+                "Todos los campos deben llenarse",Toast.LENGTH_LONG).show();
+                }else{
+                    int cantidadNew= Integer.parseInt(cantidad);
+                    int precioNew= Integer.parseInt(precio);
+                    Producto nuevo= new Producto(nombre,cantidadNew,precioNew);
+                    canasta.add(nuevo);
+                    llenarTabla(nuevo);
+                }
+            }
+        });
+
+
+
     }
+
+    public void llenarTabla(Producto product){
+        int valor= product.getPrecio()*product.getCantidad();
+        TableRow fila= new TableRow(this);
+
+        TextView cell1= new TextView(this);
+        cell1.setText(product.getNombre());
+        cell1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        fila.addView(cell1);
+
+        TextView cell2= new TextView(this);
+        cell2.setText(product.getCantidad()+"");
+        cell2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        fila.addView(cell2);
+
+        TextView cell3= new TextView(this);
+        cell3.setText(product.getPrecio()+"");
+        cell3.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        fila.addView(cell3);
+
+        TextView cell4= new TextView(this);
+        cell4.setText(valor+"");
+        cell4.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        fila.addView(cell4);
+
+        table.addView(fila);
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/registrarproductos/Producto.java
@@ -0,0 +1,38 @@
+package com.example.registrarproductos;
+
+public class Producto {
+
+    String nombre;
+    int cantidad;
+    int precio;
+
+    public Producto(String nombre, int cantidad, int precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+}
